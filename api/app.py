ebd4f892fe6ca2acf399d7ea34f5e355eb3d9c36
manjitha <manjitha.k@synergentl.com>
2025-10-21 13:23:08 +0530

    Fix langchain import and ensure clean build

--- api/app.py
+++ api/app.py
@@ -8,7 +8,7 @@ from textblob import TextBlob
 
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import FAISS
-from langchain_community.chains import ConversationalRetrievalChain
+from langchain.chains import ConversationalRetrievalChain
 from langchain_core.prompts import PromptTemplate
 
 # ------------------------------------------------------------
